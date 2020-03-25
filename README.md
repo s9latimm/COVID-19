a8e0f2ade8bdef7f6d3180dbe9e3a6416b59c646
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2020-03-25 18:09:18 +0100

    Refactor and add date support

--- README.md
+++ README.md
@@ -7,16 +7,15 @@ Please note that daily numbers only include yesterday (today's cases will appear
 ### Usage
 
 ``` 
-usage: covid.py [-h] [-c <str>] [-C <list<str>>] [-s] [-L] [-l]
-
-optional arguments:
   -h, --help            show this help message and exit
   -c <str>, --column <str>
                         one of ['cases', 'deaths']
   -C <list<str>>, --country <list<str>>
                         comma separated list of GeoIDs (e.g. "DE,US")
   -s, --show            show plot instead of saving
-  -L, --list            list available GeoIDs
+  -d <datetime>, --date <datetime>
+                        date (yyyy-mm-dd)
+  -L, --list            list available GeoIDs and exit
   -l, --log             logarithmic scale
 ```
 
@@ -24,8 +23,8 @@ optional arguments:
 
 Logarithmic plot of cases from all regions:
 
-![plots/covid-19-world-cases-log-2020-03-23.png](plots/covid-19-world-cases-log-2020-03-23.png)
+![plots/covid-19-world-cases-log-2020-03-25.svg](plots/covid-19-world-cases-log-2020-03-25.svg?sanitize=true)
 
 Linear plot of cases from Germany:
 
-![plots/covid-19-de-cases-2020-03-23.png](plots/covid-19-de-cases-2020-03-23.png)
+![plots/covid-19-de-cases-2020-03-25.svg](plots/covid-19-de-cases-2020-03-25.svg?sanitize=true)
--- covid.py
+++ covid.py
@@ -22,97 +22,207 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import requests
-import xlrd
-import datetime
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
+# -*- coding: utf-8 -*-
+"""COVID-19-GeoPlot"""
+
 import os
+import sys
+import signal
+import argparse
+import datetime
+from datetime import timedelta
 import pprint
+import matplotlib.pyplot as plt
+import xlrd
+import requests
 
 COLUMNS = {'cases': 4, 'deaths': 5}
-URL = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide'
+URL = 'https://www.ecdc.europa.eu/sites/default/files/documents/' \
+      'COVID-19-geographic-disbtribution-worldwide'
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--column', metavar='<str>', type=str, default='cases',
-                        help=f'one of {[i for i in COLUMNS.keys()]}')
-    parser.add_argument('-C', '--country', metavar='<list<str>>', type=str, default='*',
-                        help=f'comma separated list of GeoIDs (e.g. "DE,US")')
-    parser.add_argument('-s', '--show', default=False, action='store_true', help='show plot instead of saving')
-    parser.add_argument('-L', '--list', default=False, action='store_true', help='list available GeoIDs')
-    parser.add_argument('-l', '--log', default=False, action='store_true', help='logarithmic scale')
-    args = parser.parse_args()
-    today = datetime.date.today().strftime('%Y-%m-%d')
-    cnt = requests.get(f'{URL}-{today}.xlsx').content
-    table = xlrd.open_workbook(file_contents=cnt).sheet_by_index(0)
-    countries = dict()
-    for row in table.get_rows():
-        countries[row[7].value.strip()] = row[6].value.replace('_', ' ').strip()
-    countries['*'] = 'World'
-    del countries['GeoId']
-    if args.list:
-        pprint.PrettyPrinter(indent=2).pprint(countries)
-        return 0
-    if args.column not in COLUMNS.keys():
-        parser.error(f'column must be one of {[i for i in COLUMNS.keys()]}')
-    idx = COLUMNS[args.column]
-    country = sorted({i.strip() for i in args.country.strip().split(',')})
-    for c in country:
-        if c not in countries.keys():
-            parser.error(f'no data for {c}')
-    if '*' in country:
-        country = {'WORLD'}
-    raw = dict()
-    for row in table.get_rows():
-        try:
-            if 'WORLD' in country or row[7].value.strip() in country:
-                key = int(row[0].value)
-                if key in raw.keys():
-                    raw[key] += int(row[idx].value)
-                else:
-                    raw[key] = int(row[idx].value)
-        except ValueError:
-            continue
-    if not raw:
-        parser.error('no data')
-    data = [(0, 0, datetime.date.today())]
-    for i in range(min({i for i in raw.keys() if raw[i] > 0}), max(raw.keys()) + 1):
-        if i in raw.keys():
-            data.append(
-                (raw[i], data[-1][1] + raw[i], datetime.datetime(*xlrd.xldate_as_tuple(i, 0)).strftime('%Y-%m-%d')))
+class GeoPlot:
+    """Visualize up-to-date case data published by the ECDC."""
+    def __init__(self):
+        self.date = None
+        self.parser = argparse.ArgumentParser()
+        self.args = None
+        self.countries = None
+
+    def poll(self):
+        """Get raw sheet from ECDC server."""
+        rep = 0
+        while True:
+            url = f'{URL}-{self.date.strftime("%Y-%m-%d")}.xlsx'
+            try:
+                req = requests.get(url)
+            except requests.exceptions.ConnectionError as err:
+                self.parser.error(err)
+            print(f'[GET] {url} {req.status_code}')
+            if req.status_code == 200:
+                break
+            if rep > 3:
+                self.parser.error('could not get dataset')
+            elif rep == 3:
+                self.date = datetime.date.today()
+            else:
+                self.date -= timedelta(days=1)
+            rep += 1
+        return req.content
+
+    def parse_args(self):
+        """Parse options."""
+        self.parser.add_argument('-c',
+                                 '--column',
+                                 metavar='<str>',
+                                 type=str,
+                                 default='cases',
+                                 help=f'one of {list(COLUMNS.keys())}')
+        self.parser.add_argument(
+            '-C',
+            '--country',
+            metavar='<list<str>>',
+            type=str,
+            default='*',
+            help=f'comma separated list of GeoIDs (e.g. "DE,US")')
+        self.parser.add_argument('-s',
+                                 '--show',
+                                 default=False,
+                                 action='store_true',
+                                 help='show plot instead of saving')
+        self.parser.add_argument(
+            '-d',
+            '--date',
+            metavar='<datetime>',
+            type=str,
+            default=datetime.date.today().strftime('%Y-%m-%d'),
+            help=f'date (yyyy-mm-dd)')
+        self.parser.add_argument('-L',
+                                 '--list',
+                                 default=False,
+                                 action='store_true',
+                                 help='list available GeoIDs and exit')
+        self.parser.add_argument('-l',
+                                 '--log',
+                                 default=False,
+                                 action='store_true',
+                                 help='logarithmic scale')
+        self.args = self.parser.parse_args()
+
+    @staticmethod
+    def get_regions(table):
+        """Collect all available GeoIDs."""
+        countries = dict()
+        for row in table.get_rows():
+            countries[row[7].value.strip()] = row[6].value.replace(
+                '_', ' ').strip()
+        countries['*'] = 'World'
+        del countries['GeoId']
+        return countries
+
+    def get_data(self, table):
+        """Extract data from table."""
+        raw = dict()
+        idx = COLUMNS[self.args.column]
+        for row in table.get_rows():
+            try:
+                if 'WORLD' in self.countries or row[7].value.strip(
+                ) in self.countries:
+                    key = int(row[0].value)
+                    if key in raw.keys():
+                        raw[key] += int(row[idx].value)
+                    else:
+                        raw[key] = int(row[idx].value)
+            except ValueError:
+                continue
+        if not raw:
+            self.parser.error('no data')
+        data = [(0, 0, self.date)]
+        for i in range(min({i
+                            for i in raw if raw[i] > 0}),
+                       max(raw.keys()) + 1):
+            if i in raw.keys():
+                data.append(
+                    (raw[i], data[-1][1] + raw[i],
+                     datetime.datetime(*xlrd.xldate_as_tuple(i, 0)).strftime(
+                         '%Y-%m-%d')))
+            else:
+                data.append(
+                    (0, data[-1][1],
+                     datetime.datetime(*xlrd.xldate_as_tuple(i, 0)).strftime(
+                         '%Y-%m-%d')))
+
+        return data
+
+    @staticmethod
+    def handler(*_):
+        """Handle SIGINT and SIGKILL."""
+        plt.close()
+
+    def plot(self, data):
+        """Show or save plot of data."""
+        fig, ax1 = plt.subplots()
+        color = 'tab:blue'
+        ax2 = ax1.twinx()
+        ax2.plot([i[2] for i in data[1:]], [i[1] for i in data[1:]],
+                 'o-',
+                 color=color)
+        ax2.set_ylabel(self.args.column, color=color)
+        if self.args.log:
+            ax2.set_yscale('log')
         else:
-            data.append((0, data[-1][1], datetime.datetime(*xlrd.xldate_as_tuple(i, 0)).strftime('%Y-%m-%d')))
-    fig, ax1 = plt.subplots()
-    color = 'tab:blue'
-    ax2 = ax1.twinx()
-    ax2.plot([i[2] for i in data[1:]], [i[1] for i in data[1:]], 'o-', color=color)
-    ax2.set_ylabel(args.column, color=color)
-    if args.log:
-        ax2.set_yscale('log')
-    else:
-        ax2.set_ylim(bottom=0)
-    ax2.tick_params(axis='y', labelcolor=color)
-    color = 'tab:red'
-    ax1.bar([i[2] for i in data[1:]], [i[0] for i in data[1:]], color=color)
-    ax1.set_ylabel(f'new {args.column} per day', color=color)
-    ax1.set_ylim(bottom=0)
-    ax1.tick_params(axis='y', labelcolor=color)
-    ax1.tick_params(axis='x', labelrotation=90)
-    ax1.set_title(f'COVID-19 {" ".join(country)} ({data[1][2]} → {data[-1][2]})')
-    fig.set_size_inches(20, 10, forward=True)
-    fig.tight_layout()
-    if args.show:
-        plt.show()
-    else:
-        file = f'plots/covid-19-{"-".join(country)}-{args.column}{"-log" if args.log else ""}-{today}.png'.lower()
-        print(f'saving {os.path.abspath(file)}')
-        plt.savefig(file)
-    plt.close()
-    return 0
+            ax2.set_ylim(bottom=0)
+        ax2.tick_params(axis='y', labelcolor=color)
+        color = 'tab:red'
+        ax1.bar([i[2] for i in data[1:]], [i[0] for i in data[1:]],
+                color=color)
+        ax1.set_ylabel(f'new {self.args.column} per day', color=color)
+        ax1.set_ylim(bottom=0)
+        ax1.tick_params(axis='y', labelcolor=color)
+        ax1.tick_params(axis='x', labelrotation=90)
+        ax1.set_title(
+            f'COVID-19 {" ".join(self.countries)} ({data[1][2]} → {data[-1][2]})'
+        )
+        fig.set_size_inches(20, 10, forward=True)
+        fig.tight_layout()
+        if self.args.show:
+            signal.signal(signal.SIGINT, self.handler)
+            signal.signal(signal.SIGTERM, self.handler)
+            plt.show()
+        else:
+            file = f'plots/covid-19-{"-".join(self.countries)}-{self.args.column}' \
+                   f'{"-log" if self.args.log else ""}' \
+                   f'-{self.date.strftime("%Y-%m-%d")}.svg'.lower()
+            print(f'[SAVE] {os.path.abspath(file)}')
+            plt.savefig(file)
+        print('[CLOSE]')
+        plt.close()
+
+    def main(self):
+        """Entry point."""
+        self.parse_args()
+        try:
+            self.date = datetime.datetime.strptime(self.args.date, '%Y-%m-%d')
+        except ValueError as err:
+            self.parser.error(err)
+        table = xlrd.open_workbook(file_contents=self.poll()).sheet_by_index(0)
+        if self.args.list:
+            pprint.PrettyPrinter(indent=2).pprint(self.get_regions(table))
+            return 0
+        if self.args.column not in COLUMNS.keys():
+            self.parser.error(f'column must be one of {list(COLUMNS)}')
+        self.countries = sorted(
+            {i.strip()
+             for i in self.args.country.strip().split(',')})
+        regions = self.get_regions(table)
+        for country in self.countries:
+            if country not in regions.keys():
+                self.parser.error(f'no data for {country}')
+        if '*' in self.countries:
+            self.countries = {'WORLD'}
+        self.plot(self.get_data(table))
+        return 0
 
 
 if __name__ == '__main__':
-    exit(main())
+    sys.exit(GeoPlot().main())
